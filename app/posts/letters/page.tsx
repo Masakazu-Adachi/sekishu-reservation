8f61a8dfdf360a51520c521be19439672e9f7493
Masakazu-Adachi <m-adachi@sustirel.com>
2025-08-05 13:34:23 +0900

    fix: ごあいさつと同じフォントを適用

--- app/posts/letters/page.tsx
+++ app/posts/letters/page.tsx
@@ -21,9 +21,11 @@ export default function LettersPage() {
   }, []);
 
   return (
-    <main className="p-6 max-w-5xl mx-auto">
+    <main className="p-6 max-w-5xl mx-auto font-serif">
       <LinkBackToHome />
-      <h1 className="text-2xl font-bold mb-6 text-center">通信ページ</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center font-serif">
+        通信ページ
+      </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
           <BlogCard key={post.id} post={post} href={`/posts/letters/${post.id}`} />
