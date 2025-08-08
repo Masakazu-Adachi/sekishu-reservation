048a00111bc73670247a173a2767b51be781a815
Masakazu-Adachi <m-adachi@sustirel.com>
2025-08-08 20:11:23 +0900

    chore: Firebase 設定を環境変数から読み込む

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvCJhb71RoHhw5KrKTK1ZUdpceuO8NXpw",
-  authDomain: "ochakai-reserve.firebaseapp.com",
-  projectId: "ochakai-reserve",
-  storageBucket: "ochakai-reserve.appspot.com",
-  messagingSenderId: "627049774917",
-  appId: "1:627049774917:web:d7873398b9b2986d88028c",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
