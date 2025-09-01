656e4d90d169900fcf17e7141ce01a4db09be93b
Masakazu-Adachi <m-adachi@sustirel.com>
2025-09-02 01:00:51 +0900

    chore: migrate editor to react-quill v2

--- polyfills/react-dom-default.ts
+++
@@ -1,5 +0,0 @@
-import * as ReactDOMNS from "react-dom";
-// Provide a default export mirroring the named exports
-export default ReactDOMNS;
-// Re-export named exports transparently
-export * from "react-dom";
